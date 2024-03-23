06e6e8f811b4915a7aaed3983fd6083e15cec758
SdevWeb7 <152064117+SdevWeb7@users.noreply.github.com>
2024-03-23 23:07:25 +0100

    add types/node

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,12 @@ const prismaClientSingleton = () => {
    return new PrismaClient()
 }
 
+declare global {
+   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
